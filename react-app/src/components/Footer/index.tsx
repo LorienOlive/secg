066c1ac36a891d6865513fa3e93168e9d62aa4b6
Lorien Olive <lorien.olive@gmail.com>
2021-01-18 17:22:55 -0500

    Updates copyright reinstalls yarn to clear CSP error in console.

--- react-app/src/components/Footer/index.tsx
+++ react-app/src/components/Footer/index.tsx
@@ -10,10 +10,7 @@ const Footer: React.SFC<any> = props => {
   return (
     <footer className="footer">
       <div className="footer-text-container">
-        <p>
-          &copy; Copyright 2020{' '}
-          <a href="https://twitter.com/AnuragG94634191">Anurag Garg</a>
-        </p>
+        <p>&copy; Copyright 2020 Lorien Olive</p>
       </div>
     </footer>
   );
